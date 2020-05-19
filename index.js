8b7e0c1afe2a0b74b4f45aa64255c1d3a68d656a
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-19 13:30:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveDodgerLeft() {
 function moveDodgerRight () {
   let rightNumbers = dodger.style.left.replace("px", "");
   let right = parseInt(rightNumbers, 10);
-  if ( right < 360) {
+  if ( right > 0) {
     dodger.style.left = `${right + 1}px `;
     
   }
